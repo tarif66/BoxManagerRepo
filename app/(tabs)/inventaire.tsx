b542519d5457f3e80b205c592122f36778312460
tarif66 <obrechon@gmail.com>
2024-11-06 16:28:49 +0100

    1ere carte

--- app/(tabs)/inventaire.tsx
+++ app/(tabs)/inventaire.tsx
@@ -1,21 +1,35 @@
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
+
 
 export default function TabTwoScreen() {
+  const [location, setLocation] = React.useState(null);
+
   return (
     <View style={s.container}>
-      <Text style={s.text}>Inventaire
-      </Text>
+      <MapView 
+        style={s.map} 
+        initialRegion={{
+          latitude: 49.2441,       // Set the initial latitude
+          longitude: 4.0408,     // Set the initial longitude
+          latitudeDelta: 1.5,    // Set the latitude span for the viewport
+          longitudeDelta: 1.5,   // Set the longitude span for the viewport
+        }}
+        showsUserLocation={true}
+        showsMyLocationButton={true}
+        />
+      
     </View>
   );
 }
 
 const s = StyleSheet.create({
   container: {
-    alignItems: 'center',
-    justifyContent: 'center',
     flex: 1,
   },
-  text: {
-    alignItems: 'center',
+  map: {
+    width: '100%',
+    height: '100%',
   },
 });
