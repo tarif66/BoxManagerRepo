34452e21795b62f45ce05f378c2062d095f5123f
tarif66 <obrechon@gmail.com>
2024-11-15 12:10:12 +0100

    Lien carte et ouverture modale

--- app/(tabs)/boxes.tsx
+++ app/(tabs)/boxes.tsx
@@ -1,26 +1,66 @@
-import { StyleSheet,View, ScrollView } from 'react-native';
+import { StyleSheet,View, ScrollView, Pressable, Modal, Text, SafeAreaView, Alert } from 'react-native';
 import React, { useState } from 'react';
+import { useNavigation } from '@react-navigation/native';
 
 import BoxCard from "../../components/box-card"
 import Box from "../../components/models/box"
 import { BOXES } from "../../components/models/mock-boxes"
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { Link } from 'expo-router';
 
 export default function TabTwoScreen() {
-    const [boxes, setBoxes] = useState<Box[]>(BOXES);
-
+  const [modalVisible, setModalVisible] = useState(false);
+  const [boxes, setBoxes] = useState<Box[]>(BOXES);
+  
   return (
-    <ScrollView>
-        <View style={s.container}>
-            <View style={s.row}>
-            {boxes.map(boxMap => (
-                <BoxCard key={boxMap.id} boxProp={boxMap}/>
-            ))}
+    <SafeAreaProvider>
+      <SafeAreaView style={styles.centeredView}>
+        <Modal
+          animationType="slide"
+          transparent={true}
+          visible={modalVisible}
+          onRequestClose={() => {
+            Alert.alert('Modal has been closed.');
+            setModalVisible(!modalVisible);
+          }}>
+          <View style={styles.centeredView}>
+            <View style={styles.modalView}>
+              <Text style={styles.modalText}>Hello World!</Text>
+              <Pressable
+                style={[styles.button, styles.buttonClose]}
+                onPress={() => setModalVisible(!modalVisible)}>
+                <Text style={styles.textStyle}>Hide Modal</Text>
+              </Pressable>
             </View>
+          </View>
+        </Modal>
         
-        </View>
-    </ScrollView>
+        <ScrollView>
+          <View style={s.container}>
+              <View style={s.row}>
+              {boxes.map(boxMap => (
+                  <View>
+                    <BoxCard key={boxMap.id} boxProp={boxMap}/>
+                    <Pressable
+                    style={[styles.button, styles.buttonOpen]}
+                    onPress={() => setModalVisible(true)}>
+                    <Text style={styles.textStyle}>Show Modal</Text>
+                    </Pressable>
+                    <Pressable
+                      style={[styles.button, styles.buttonOpen]}>
+                      <Link style={styles.textStyle} href="/(tabs)/map">Show Map</Link>
+                    </Pressable>
+                  </View>
+              ))}
+              </View>
+          
+          </View>
+
+        </ScrollView>
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
-}
+};
 
 const s = StyleSheet.create({
   container: {
@@ -35,5 +75,87 @@ const s = StyleSheet.create({
     flexWrap: 'wrap',
     justifyContent: 'center',
     gap: 16,
+  }});
+
+
+const styles = StyleSheet.create({
+  centeredView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 35,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+  },
+  buttonOpen: {
+    backgroundColor: '#F194FF',
+  },
+  buttonClose: {
+    backgroundColor: '#2196F3',
+  },
+  textStyle: {
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: 'center',
   },
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
